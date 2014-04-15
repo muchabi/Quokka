2561d90d4db36734ec34e67e5ae95d5dcf002322
ahsiao <andrew.a.hsiao@gmail.com>
2014-04-15 19:52:05 -0400

    Hooked up main menu to notes menu

---
+++ public/js/mainmenu.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("#btn-phone").on("click", function(){
+    alert("Hook this up to Tommy's code.");
+  });
+  
+  $("#btn-notes").on("click", function(){
+    location.href="/d/notesmenu";
+  });
+});
